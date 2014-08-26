414d818d2fedce337e22f7cc4be735cf76158a25
Chris Holcombe <xfactor973@gmail.com>
2014-08-25 20:30:00 -0400

    First

---
+++ README.md
@@ -0,0 +1,1 @@
+Part of the "What I did in college series" This was me playing around with double buffering while learning how animations worked in swing.  I assume I was working towards learning how to program a video game but I never got there.
---
+++ src/buffering/XPoint.java
@@ -0,0 +1,21 @@
+package buffering;
+
+public class XPoint {
+  public int x,y;
+
+  public XPoint(){
+    x=0;
+    y=0;
+  }
+  public XPoint(int x,int y){
+    this.x = x;
+    this.y = y;
+  }
+  public int getX(){
+    return x;
+  }
+  public int getY(){
+    return y;
+  }
+
+}
